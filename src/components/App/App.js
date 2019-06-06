3c539fad832ad7efc17d4cb78e03fe708af54982
devsigner-sunny <devsigner.sunny@gmail.com>
2019-06-06 22:44:20 +1200

    apply pure-react-carousel & node-sass-chokidar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import 'components/App/App.css';
+import { CarouselProvider, Slider, Slide, ButtonNext} from 'pure-react-carousel';
+import Section2 from 'components/section2'
+
 
 function App() {
   return (
     <div className="App">
-      TOP
+        <CarouselProvider
+         visibleSlides={1}
+         orientation="vertical"
+         totalSlides={3}
+         step={1}
+         touchEnabled={false}
+      >
+        <Slider>
+          <Slide index={0}>
+          <div>
+            <div className="container-fluid">
+              I am the first Slide.
+              <ButtonNext/>
+            </div>
+          </div>
+          </Slide>
+          <Slide index={1}>I am the second Slide.</Slide>
+          <Slide index={2}>I am the third Slide.</Slide>
+        </Slider>
+      </CarouselProvider>
+        <Section2 />
     </div>
   );
 }
