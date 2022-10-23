e71f50daf4383b72d99731d5befdf52651863cb1
devsigner-sunny <devsigner.sunny@gmail.com>
2022-10-23 21:13:26 +1300

    create content.js + remove unncessary static link

---
+++ src/content.js
@@ -0,0 +1,31 @@
+const logoText = 'Sunny';
+const meta = {
+  title: 'Sunny Kim',
+  description: "I'm Sunny Kim front-end developer with a strong web design background. currently working in Auckland.",
+};
+
+const socialLinks = {
+  github: 'https://github.com/devsigner-sunny/',
+  linkedin: 'https://www.linkedin.com/in/devsigner-sunny/',
+  web: 'https://devsigner-sunny.github.io/react-portfolio',
+};
+
+const introData = {
+  title: 'Nice to see you!',
+  animated: {
+    first: 'DEVeloper + deSIGNER',
+    second: "I'm a devsginer",
+    third: 'I love coding and cool design',
+  },
+  description: 'An experienced Front-end Developer with 3 years of development experience with a strong web design background. \n'
+    + '\n'
+    + 'Building state-of-the-art, easy-to-use, user-friendly websites and applications is truly a passion of mine \n'
+    + '\n',
+};
+
+export {
+  logoText,
+  meta,
+  socialLinks,
+  introData,
+};
