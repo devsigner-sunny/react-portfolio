bd97abf532239ce91393305233dc57ab4b074d0b
Sunny Kim <devsigner.sunny@gmail.com>
2023-03-15 20:02:03 +1300

    add footer

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import AppRoutes from "./Routes";
+import { ParallaxProvider } from "react-scroll-parallax";
 import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
 import "./App.scss";
 
 export default function App() {
   return (
-    <>
+    <ParallaxProvider>
       <Header />
       <div className="px-32 py-20">
         <AppRoutes />
       </div>
-    </>
+      <Footer />
+    </ParallaxProvider>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useRef, useLayoutEffect } from "react";
+import symbolMoon from "../../Assets/svg/moon.svg";
+import symbolFingerLight from "../../Assets/svg/finger-light.svg";
+import { socialLinks } from "../../Contents/siteContents";
+import "./Footer.scss";
+
+function Footer() {
+  // console.log(`:`, Object.values(socialLinks));
+  console.log(`:`, Object.entries(socialLinks)[0][0]);
+  const socialLinkList = Object.entries(socialLinks);
+  return (
+    <footer
+      className="flex items-end p-20 mt-10 bg-no-repeat section-cta bg__dark"
+      style={{
+        backgroundImage: `url(${symbolMoon})`,
+      }}
+    >
+      <div>
+        <h4 className="mb-2 font-medium text__light">Wanna know more?</h4>
+        <div className="flex items-center">
+          <h2 className="inline text__light">Send me a message</h2>
+          <img
+            src={symbolFingerLight}
+            className="inline w-12 ml-6 img-finger animate__animated animate__shakeX animate__infinite"
+          />
+        </div>
+      </div>
+      <ul className="flex gap-5 ml-auto mr-32">
+        {socialLinkList?.map((link, index) => (
+          <li key={index}>
+            <a
+              className="tracking-wider uppercase text__light"
+              href={link[1]}
+              target="_blank"
+              rel="noopenner noreferrer"
+            >
+              {link[0]}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
