0d0fc7d85eb684d46c85c9ab3e2bfdf90f3d0ecd
Sunny Kim <devsigner.sunny@gmail.com>
2023-03-22 18:12:32 +1300

    install gsap, add MousePointer

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -4,10 +4,12 @@ import Header from "./Layout/Header/Header";
 import Footer from "./Layout/Footer/Footer";
 import "./App.scss";
 import NavProvider from "../Context/NavContext";
+import MousePointer from "../Components/MousePointer";
 
 export default function App() {
   return (
     <NavProvider>
+      <MousePointer />
       <Header />
       <Main />
       <Footer />
---
+++ src/Components/MousePointer.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useRef, useImperativeHandle, forwardRef } from "react";
+import { gsap } from "gsap";
+
+const MouseCircle = forwardRef(({ className, delay }, ref) => {
+  const el = useRef();
+  const mouseCircleClasses = [
+    "mouse-circle",
+    "top-0",
+    "left-0",
+    "fixed",
+    "rounded-full",
+    "transform__x-y-center",
+    "border-2",
+  ];
+
+  mouseCircleClasses.push(className);
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        moveTo(x, y) {
+          gsap.to(el.current, { x, y, delay });
+        },
+      };
+    },
+    [delay]
+  );
+
+  return <div className={mouseCircleClasses.join(" ")} ref={el}></div>;
+});
+
+MouseCircle.displayName = "MouseCircle";
+
+export default function MousePointer() {
+  const circleRefs = useRef([]);
+
+  // reset on re-renders
+  circleRefs.current = [];
+
+  useEffect(() => {
+    const { innerWidth, innerHeight } = window;
+    circleRefs.current.forEach((ref) =>
+      ref.moveTo(innerWidth / 2, innerHeight / 2)
+    );
+
+    const onMove = ({ clientX, clientY }) => {
+      circleRefs.current.forEach((ref) => ref.moveTo(clientX, clientY));
+    };
+
+    window.addEventListener("pointermove", onMove);
+
+    return () => {
+      window.removeEventListener("pointermove", onMove);
+    };
+  }, []);
+
+  const addCircleRef = (ref) => {
+    if (ref) {
+      circleRefs.current.push(ref);
+    }
+  };
+
+  return (
+    <div>
+      <MouseCircle
+        className="z-50 w-3.5 h-3.5 bg__primary"
+        ref={addCircleRef}
+        delay={0}
+      />
+      <MouseCircle
+        className="z-40 w-10 h-10 border__light opacity-30 bg__primary"
+        ref={addCircleRef}
+        delay={0.1}
+      />
+      <MouseCircle
+        className="z-30 w-20 h-20 border__primary bg__light"
+        ref={addCircleRef}
+        delay={0.2}
+      />
+    </div>
+  );
+}
